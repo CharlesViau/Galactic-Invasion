1b1ffcfdb816b17b92e420746023ec8bd7e4c8d0
Samuel Legendre <samuel.legendre@protonmail.com>
2023-02-20 20:33:19 -0500

    Create destruction prototype

---
+++ Assets/SamTest/Scripts/Destructible.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject fracturedPlanet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Instantiate(fracturedPlanet, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
