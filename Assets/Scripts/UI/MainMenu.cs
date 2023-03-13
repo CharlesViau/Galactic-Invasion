241c7a7a6947dda6ceced9df7646ed410224e118
JuanCastilla <juanjocama2014@gmail.com>
2023-03-13 19:47:14 -0400

    Options presque finis
    J'ai reussis a terminer quelque ajustements c'Est a dire les slider du volume, luminosité et sensibilité de la manette. Reste juste a confirmer a mettre le code pour le fullscreen et windowed pis le leaderboard. Demain je continue

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,22 +7,30 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("Volume Setting")] 
+    [Header("Volume Setting")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
-    [SerializeField] private float defaultVolume = 1.0f;
+    [SerializeField] private float defaultVolume = 0.5f;
 
-    
-    [Header("Confirmation")] 
+    [Header("Brightness Setting")]
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private float defaultBrightness = 0.5f;
+
+    [Header("Sensitivity Setting")]
+    [SerializeField] private TMP_Text sensitivityTextValue = null;
+    [SerializeField] private Slider sensitivitySlider = null;
+    [SerializeField] private int defaultSensitivity = 4;
+
+    [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
 
-    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGAme()
+    public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
@@ -34,18 +42,78 @@ public class MainMenu : MonoBehaviour
         volumeTextValue.text = volume.ToString("0.0");
     }
 
-    public void VolumeApply()
+    public void SetBrightness(float brightness)
+    {
+        if (brightnessTextValue != null && brightnessSlider != null) {
+            RenderSettings.ambientIntensity = brightness;
+            brightnessTextValue.text = brightness.ToString("0.0");
+        }
+    }
+
+    public void SetSensitivity(float sensitivity)
+    {
+        if (sensitivityTextValue != null) {
+            int roundedSensitivity = Mathf.RoundToInt(sensitivity);
+            sensitivityTextValue.text = roundedSensitivity.ToString();
+        }
+    }
+
+
+    public void ApplySettings()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        PlayerPrefs.SetFloat("brightness", RenderSettings.ambientIntensity);
+        PlayerPrefs.SetFloat("sensitivity", sensitivitySlider.value);
         StartCoroutine(ConfirmationBox());
     }
 
-    public void ResetButton(string MenuType)
+    public void ResetSettings()
     {
-        AudioListener.volume = defaultVolume;
-        volumeSlider.value = defaultVolume;
-        volumeTextValue.text = defaultVolume.ToString("0.0");
-        VolumeApply();
+        SetVolume(defaultVolume);
+        SetBrightness(defaultBrightness);
+        SetSensitivity(defaultSensitivity);
+
+        if (volumeSlider != null) {
+            volumeSlider.value = defaultVolume;
+        }
+        if (brightnessSlider != null) {
+            brightnessSlider.value = defaultBrightness;
+        }
+        if (sensitivitySlider != null) {
+            sensitivitySlider.value = defaultSensitivity;
+        }
+
+        PlayerPrefs.SetFloat("masterVolume", defaultVolume);
+        PlayerPrefs.SetFloat("brightness", defaultBrightness);
+        PlayerPrefs.SetInt("sensitivity", defaultSensitivity);
+    }
+
+    public void LoadSettings()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("masterVolume", defaultVolume);
+        SetVolume(savedVolume);
+
+        float savedBrightness = PlayerPrefs.GetFloat("brightness", defaultBrightness);
+        SetBrightness(savedBrightness);
+
+        int savedSensitivity = PlayerPrefs.GetInt("sensitivity", defaultSensitivity);
+        SetSensitivity(savedSensitivity);
+
+        if (volumeSlider != null) {
+            volumeSlider.value = savedVolume;
+        }
+        if (brightnessSlider != null) {
+            brightnessSlider.value = savedBrightness;
+        }
+        if (sensitivitySlider != null) {
+            sensitivitySlider.value = savedSensitivity;
+        }
+    }
+    
+    public void BackButton()
+    {
+        LoadSettings();
+        // Add any other actions you want to perform when the back button is clicked
     }
 
     public IEnumerator ConfirmationBox()
@@ -55,3 +123,4 @@ public class MainMenu : MonoBehaviour
         confirmationPrompt.SetActive(false);
     }
 }
+
