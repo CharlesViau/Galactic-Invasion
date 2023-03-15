2cd5496a53259e1cef275e0dbde281d859580bbf
JuanCastilla <juanjocama2014@gmail.com>
2023-03-14 21:28:32 -0400

    MenuAjuster
    Jai du reajuster la resolution pcq qd je builder et je tester sa marcher pas bien. Mtn la resolution marche bien (1920x1080 px). Reste juste a designer et a integrer le code a sam

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -12,16 +12,6 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Slider volumeSlider = null;
     [SerializeField] private float defaultVolume = 0.5f;
 
-    [Header("Brightness Setting")]
-    [SerializeField] private TMP_Text brightnessTextValue = null;
-    [SerializeField] private Slider brightnessSlider = null;
-    [SerializeField] private float defaultBrightness = 0.5f;
-
-    [Header("Sensitivity Setting")]
-    [SerializeField] private TMP_Text sensitivityTextValue = null;
-    [SerializeField] private Slider sensitivitySlider = null;
-    [SerializeField] private int defaultSensitivity = 4;
-
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
 
@@ -40,86 +30,64 @@ public class MainMenu : MonoBehaviour
     {
         AudioListener.volume = volume;
         volumeTextValue.text = volume.ToString("0.0");
+        volumeSlider.SetValueWithoutNotify(volume);
     }
-
-    public void SetBrightness(float brightness)
-    {
-        if (brightnessTextValue != null && brightnessSlider != null) {
-            RenderSettings.ambientIntensity = brightness;
-            brightnessTextValue.text = brightness.ToString("0.0");
-        }
-    }
-
-    public void SetSensitivity(float sensitivity)
-    {
-        if (sensitivityTextValue != null) {
-            int roundedSensitivity = Mathf.RoundToInt(sensitivity);
-            sensitivityTextValue.text = roundedSensitivity.ToString();
-        }
-    }
-
 
     public void ApplySettings()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        PlayerPrefs.SetFloat("brightness", RenderSettings.ambientIntensity);
-        PlayerPrefs.SetFloat("sensitivity", sensitivitySlider.value);
         StartCoroutine(ConfirmationBox());
     }
 
     public void ResetSettings()
     {
         SetVolume(defaultVolume);
-        SetBrightness(defaultBrightness);
-        SetSensitivity(defaultSensitivity);
 
         if (volumeSlider != null) {
             volumeSlider.value = defaultVolume;
         }
-        if (brightnessSlider != null) {
-            brightnessSlider.value = defaultBrightness;
-        }
-        if (sensitivitySlider != null) {
-            sensitivitySlider.value = defaultSensitivity;
-        }
 
         PlayerPrefs.SetFloat("masterVolume", defaultVolume);
-        PlayerPrefs.SetFloat("brightness", defaultBrightness);
-        PlayerPrefs.SetInt("sensitivity", defaultSensitivity);
     }
 
     public void LoadSettings()
     {
         float savedVolume = PlayerPrefs.GetFloat("masterVolume", defaultVolume);
         SetVolume(savedVolume);
-
-        float savedBrightness = PlayerPrefs.GetFloat("brightness", defaultBrightness);
-        SetBrightness(savedBrightness);
-
-        int savedSensitivity = PlayerPrefs.GetInt("sensitivity", defaultSensitivity);
-        SetSensitivity(savedSensitivity);
-
-        if (volumeSlider != null) {
-            volumeSlider.value = savedVolume;
-        }
-        if (brightnessSlider != null) {
-            brightnessSlider.value = savedBrightness;
-        }
-        if (sensitivitySlider != null) {
-            sensitivitySlider.value = savedSensitivity;
-        }
     }
-    
+
     public void BackButton()
     {
         LoadSettings();
         // Add any other actions you want to perform when the back button is clicked
     }
 
+    public void SetFullscreenMode()
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+    }
+
+    public void SetWindowedMode()
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
+    }
+
+    public void OnFullscreenToggleValueChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            SetFullscreenMode();
+        }
+        else
+        {
+            SetWindowedMode();
+        }
+    }
+
     public IEnumerator ConfirmationBox()
     {
         confirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         confirmationPrompt.SetActive(false);
     }
 }
