f1648e7cc23aa0a9528ecafc30c64dd665764ba9
Samuel Legendre <samuel.legendre@protonmail.com>
2023-03-17 11:33:29 -0400

    Implement score system with UI

---
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Motherbase
+{
+    public class PlayerScore : MonoBehaviour
+    {
+        private float _score;
+
+        private void Start()
+        {
+            _score = 0;
+        }
+
+        private void Update()
+        {
+            _score += Time.deltaTime;
+        }
+
+        public int GetScore()
+        {
+            return (int)_score;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PlayerScoreUI.cs
@@ -0,0 +1,24 @@
+﻿using Motherbase;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class PlayerScoreUI : MonoBehaviour
+    {
+        public GameObject player;
+        private PlayerScore _playerScore;
+        private TextMeshProUGUI _scoreText;
+
+        private void Start()
+        {
+            _playerScore = player.GetComponent<PlayerScore>();
+            _scoreText = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            _scoreText.text = $"Score: {_playerScore.GetScore()}";
+        }
+    }
+}
