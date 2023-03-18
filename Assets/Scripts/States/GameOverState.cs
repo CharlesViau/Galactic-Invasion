6a6e67db97efe181c85d6e7921ce31ca112b15f9
Samuel Legendre <samuel.legendre@protonmail.com>
2023-03-17 20:01:18 -0400

    Create game over scene

---
+++ Assets/Scripts/States/GameOverState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Core;
+
+namespace States
+{
+    public class GameOverState : State
+    {
+        public override void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
