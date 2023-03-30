73efbea14b430050f7a8cd3ba36799ebf7a55c6f
KEVPC\keven <kevenlessard23@gmail.com>
2023-03-29 23:44:23 -0400

    Added tempoPlanet into ability

--- Assets/Scripts/Player/Hand.cs
+++ Assets/Scripts/Player/Hand.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Motherbase;
+using Planets;
 using UnityEngine;
 
+enum CurrentAbility
+{
+    None,
+    BlackHole,
+    TempoPlanet
+}
 public class Hand : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
@@ -12,9 +20,11 @@ public class Hand : MonoBehaviour
     [SerializeField] private GameObject tempoPreview;
     [SerializeField] private GameObject tempo;
 
-    private GameObject blackHolePreviewRef;
+    private GameObject previewRef;
 
     private bool isPlacingAbilty = false;
+
+    private CurrentAbility _currentAbility;
     private void Update()
     {
         var currentPos = Input.mousePosition;
@@ -25,13 +35,41 @@ public class Hand : MonoBehaviour
 
         if (isPlacingAbilty)
         {
-            blackHolePreviewRef.transform.position = referenceTransform.position;
-
+            previewRef.transform.position = referenceTransform.position;
             if (Input.GetMouseButtonDown(0))
             {
-                SpawnBlackHole();
+                switch (_currentAbility)
+                {
+                    case CurrentAbility.BlackHole:
+                        SpawnBlackHole();
+                        break;
+                    case CurrentAbility.TempoPlanet:
+                        SpawnTempoPlanet();
+                        break;
+                }
+
+                _currentAbility = CurrentAbility.None;
             }
         }
+    }
+    
+    public void OnTempoPlanetAbilityClick()
+    {
+        if (isPlacingAbilty) return;
+        if (PlayerCurrency.Instance.SpendMoney(PlayerCurrency.Instance.tempoPlanetCost))
+        {
+            _currentAbility = CurrentAbility.TempoPlanet;
+            isPlacingAbilty = true;
+            previewRef = Instantiate(tempoPreview,
+                referenceTransform.position, tempoPreview.transform.rotation);
+        }
+    }
+    
+    private void SpawnTempoPlanet()
+    {
+        isPlacingAbilty = false;
+        Destroy(previewRef.gameObject);
+        Instantiate(tempo, referenceTransform.position, tempo.transform.rotation);
     }
 
     public void OnBlackHoleAbilityClick()
@@ -39,8 +77,9 @@ public class Hand : MonoBehaviour
         if (isPlacingAbilty) return;
         if (PlayerCurrency.Instance.SpendMoney(PlayerCurrency.Instance.blackHoleCost))
         {
+            _currentAbility = CurrentAbility.BlackHole;
             isPlacingAbilty = true;
-            blackHolePreviewRef = Instantiate(blackHolePreview,
+            previewRef = Instantiate(blackHolePreview,
                 referenceTransform.position, blackHolePreview.transform.rotation);
         }
     }
@@ -48,7 +87,7 @@ public class Hand : MonoBehaviour
     private void SpawnBlackHole()
     {
         isPlacingAbilty = false;
-        Destroy(blackHolePreviewRef.gameObject);
+        Destroy(previewRef.gameObject);
         Instantiate(blackHole, referenceTransform.position, blackHole.transform.rotation);
     }
 }
