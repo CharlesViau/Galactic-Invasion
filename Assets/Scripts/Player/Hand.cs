b403f532154a257161dcf5d7e70619e2127059bc
KEVPC\keven <kevenlessard23@gmail.com>
2023-04-05 18:21:08 -0400

    Add mouse offset

--- Assets/Scripts/Player/Hand.cs
+++ Assets/Scripts/Player/Hand.cs
@@ -46,7 +46,7 @@ public class Hand : MonoBehaviour
     {
         var currentPos = Input.mousePosition;
         var worldPos =
-            Camera.main.ScreenToWorldPoint(new Vector3(currentPos.x, currentPos.y, -Camera.main.transform.position.z));
+            Camera.main.ScreenToWorldPoint(new Vector3(currentPos.x - 70, currentPos.y + 15, -Camera.main.transform.position.z));
         worldPos.z = 0;
         playerTransform.position = worldPos;
 
