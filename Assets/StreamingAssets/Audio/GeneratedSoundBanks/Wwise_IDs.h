8e56e94b047a18ef631d51e78e75728e9e611217
Charles Viau <73210557+CharlesViau@users.noreply.github.com>
2023-04-10 21:54:15 -0400

    Wwise Integration

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID PLAY_ALERTRING = 538233354U;
+        static const AkUniqueID PLAY_BACKGROUND = 3196207338U;
+        static const AkUniqueID PLAY_LEVELUP = 1202337555U;
+        static const AkUniqueID PLAY_METEORITEDESTRUCTION = 2631163362U;
+        static const AkUniqueID PLAY_TURRETCONSTRUCTION = 3756780447U;
+        static const AkUniqueID PLAY_TURRETDESTRUCTION = 3039094228U;
+        static const AkUniqueID PLAY_TURRETSHOOT = 2727078245U;
+        static const AkUniqueID PLAY_UICONFIRM = 255120960U;
+        static const AkUniqueID PLAY_UIFAIL = 4214330844U;
+        static const AkUniqueID PLAY_UIHOVER = 2502251926U;
+        static const AkUniqueID PLAY_WAVEALERT = 3646228513U;
+        static const AkUniqueID STOP_BACKGROUND = 1244803444U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
