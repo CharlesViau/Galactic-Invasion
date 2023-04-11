a7a83742e59b8644d9bcd22ed6f030cb22f20039
Samuel Legendre <samuel.legendre@protonmail.com>
2023-04-11 12:33:27 -0400

    Reformat

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -1,29 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Event = AK.Wwise.Event;
 
 public class MenuButton : MonoBehaviour
 {
-    public AK.Wwise.Event hoverEvent;
-    public AK.Wwise.Event onClickEvent;
-    private Button button;
+    public Event hoverEvent;
+    public Event onClickEvent;
+    private Button _button;
 
     private void OnEnable()
     {
-        button = GetComponent<Button>();
+        _button = GetComponent<Button>();
     }
 
     public void Play_HoverSound()
     {
-        if (button == null || button.IsInteractable()) hoverEvent?.Post(gameObject);
+        if (_button == null || _button.IsInteractable()) hoverEvent?.Post(gameObject);
     }
 
     public void Play_OnClickSound()
     {
         onClickEvent?.Post(gameObject);
     }
-
 }
