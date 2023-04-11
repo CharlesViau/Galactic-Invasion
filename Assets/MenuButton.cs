6e017af7cc90ff4ab7b1d2fb05c9869b3e07470d
Samuel Legendre <samuel.legendre@protonmail.com>
2023-04-11 12:31:44 -0400

    Fix

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
     public AK.Wwise.Event hoverEvent;
     public AK.Wwise.Event onClickEvent;
+    private Button button;
+
+    private void OnEnable()
+    {
+        button = GetComponent<Button>();
+    }
 
     public void Play_HoverSound()
     {
-        hoverEvent?.Post(gameObject);
+        if (button == null || button.IsInteractable()) hoverEvent?.Post(gameObject);
     }
 
     public void Play_OnClickSound()
